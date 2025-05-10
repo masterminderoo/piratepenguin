474b339e95c2d5644810a45558e213cba577c630
masterminderoo <cabridged@gmail.com>
2025-05-10 16:57:32 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PiratePenguinCalendar from './components/PiratePenguinCalendar';
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-200 to-blue-500">
+      <PiratePenguinCalendar />
+    </div>
+  );
+};
+
+export default App;
